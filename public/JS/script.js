887f9cb078a09eadb83cbb57bbcbc0c43cd2cf40
Sarah Acquaye Amedior <amediorsarah@gmail.com>
2024-06-12 15:11:08 +0000

    Objects in JavaScript

--- public/JS/script.js
+++ public/JS/script.js
@@ -68,4 +68,205 @@ const bottle1 = {
   
   
   
+  // Functions
+// Defining a Function
+function login (username, password) {
+  // Validte The Username and Password
+  if (!username || !password) {
+    return 'username or password not provided';
+  }
+  // Verify Username and Password
+  if (username == 'ekua' && password == '1234')
+    { return 'user is logged in';} else { return 'Invalid username or password';   }
+}
+// Invoking a Function
+login('ekua');
+
+
+
+// Basic Arithmetic Operations
+11 + 12;
+1 + 0.5;
+43 - 12;
+45 / 7;
+9 * 9;
+14 % 3;
+
+// Built in Math Functions
+Math.floor(45 /24);
+Math.random() * 10;
+
+Math.max(34, 32, 37);
+
+// Handling User Input
+// NAN
+'2'+ '2'
+Number ('2') +2
+Number ('ty') +2
+// strings in JavaScript
+// Concatenation
+const firstname = 'ekua';
+const lastname = 'A';
+const middlename =
+firstname + ' ' + lastname;
+
+// Template literal
+`${firstname} ${lastname}`;
+
+
+
+// String Methods
+
+let fullName = "Ekua A"
+
+fullName.length
+fullName.toLowerCase()
+fullName.toUpperCase()
+fullName.charAt(3)
+fullName.slice(0,4)
+fullName.split("");
+fullName.replace("A", "Med");
+fullName.indexOf("kua");
+
+//string conversion
+Number ("3.243")
+parseInt("3.243")
+parseFloat("3.243")
+let amount = 18000
+console.log(`GHS${amount}`)
+amount.toString()
+
+
+// Write a funtion that will add a participant to our Google classroom
+// let user_email = 'example@email.com'
+// let password = 'orange'
+const participants = [];
+function addParticipant(email) {
+  // Check if email was provided NOTE: short hand "if (!email)"
+  if (email===undefined|| email===null)  {
+    return 'No email provided';
+  }
+  // Check if email is valid
+  if (!email.includes('@'&&'.com')) {
+    return 'Invalid email provided'
+  }
+  // Add email to participants
+  participants.push(email);
+  return 'Participant added!';
+}
+
+addParticipant('example@gmail.com');
+addParticipant()
+addParticipant('example@')
+// addParticipant('example@.com')
+addParticipant('example@gmail.com')
+participants;
+
+
+
+
+// Arrays In JavaScript
+const users = [
+  {
+    username: 'meddy',
+    password: '1234',
+    email: 'example@gmail.com'
+  },
+  {
+    username:'darah',
+    password:'12345',
+    email:'darah@gmail.com'
+  }
+]
+<!-- invoking arrays -->
+users;
+
+
+
+// Write a funtion that will take a user with firstname, lastname and return fullname;
+
+function fullName(user) {
+  return {...user,
+          fullName: `${user.firstname} ${user.lastname}`
+          };
+}
+
+const user = {
+  firstname: 'ekua',
+  lastname: 'A'
+}
+
+fullName(user);
+
+
+// Array map
+const users = [
+  {firstname: 'ekua', lastname:'A'},
+  {firstname: 'bella', lastname:'D'},
+  {firstname: 'afia', lastname:'Tyson'},
+  {firstname: 'ana', lastname:'Belle'},
+  {firstname: 'livy', lastname:'Devon'},
+]
+
+users.map(fullName);
+
+
+
+
+// Square of Numbers
+function square(x) {
+  return x*x        
+};
+
+square(2);
+
+const numbers = [1, 9, 9, 8];
+numbers.map(square);
+
+
+// Array filter
+
+function isEven(number) {
+  // if numbers divided by two, has no remainder
+  return number % 2 === 0;        
+}
+
+isEven(9);
+numbers.filter(isEven)
+
+function isOdd(number) {
+  // if numbers divided by two, has a remainder
+  return number % 2 !== 0;        
+}
+
+isOdd(9);
+numbers.filter(is0dd)
+
+
+
+
+
+// Write a function that will allow a user to reset their password
+// const user = {
+//   email: 'example@gmail.com',
+//   password: '1234'
+// }
+
+function resetPassword(email, newPassword) {
+  // Check if email and new password was provided
+  if (!email || !newPassword) {
+    return 'Email or Password not provided'
+  }
+  // Check if provided email is correct
+  if (email !==user.email) {
+    return 'Incorrect email'
+  }
   
+  // update password with new one
+  user.password = newPassword;
+  return 'password reset'
+}
+
+user;
+resetPassword('example@gmail.com', '99900')
+user;
